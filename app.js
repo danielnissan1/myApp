7f32687a2b822ea60cb15ba49248dff9ae218a42
unknown <danielnissan2002@gmail.com>
2024-12-02 20:42:19 +0200

    add date

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const initApp = require("./server");
 const port = process.env.PORT;
 
-console.log("1");
 initApp().then((app) => {
   app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
 });
-
-// module.exports = app;
