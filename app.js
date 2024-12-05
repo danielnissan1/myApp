46c42ebabd75dfe93428133d940ca1f0b6f64c20
danielnissan1 <124774291+danielnissan1@users.noreply.github.com>
2024-12-05 22:23:40 +0200

    Revert "Comments"

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const initApp = require("./server");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
 const port = process.env.PORT;
 
-initApp().then((app) => {
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
-});
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_CONNECT);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
-// module.exports = app;
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// const postsRoute = require("./routes/posts_route");
+// app.use("/posts", postsRoute);
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
