17c006afbb8dce10a5d1b55ae3a5f6e58a70cb10
noasasson <noasasson18@gmail.com>
2025-02-20 18:36:27 +0200

    did the logout

--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -1,12 +1,11 @@
 import React, { FC, useEffect, useState, useContext } from "react";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { IPost, instance } from "../App";
 import { UserContext } from "../context";
 import Post from "../components/post";
-import Avatar from "@mui/material/Avatar";
+import { Avatar, Button, IconButton } from "@mui/material";
+import EditIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 
 interface Props {}
 
@@ -14,57 +13,59 @@ const Profile = ({}: Props) => {
   const [userPosts, setUserPosts] = useState<IPost[]>([]);
   const userContext = useContext(UserContext);
 
-  useEffect(() => {
-    // const getUserPosts = () => {
-    //   instance
-    //     .get(`/posts/userid`)
-    //     .then((res: any) => {
-    //       // handle success
-    //       // console.log(res.data);
-    //       setUserPosts(res.data);
-    //     })
-    //     .catch((error: any) => {
-    //       // handle error
-    //       console.log(error);
-    //     })
-    //     .finally(() => {
-    //       // always executed
-    //     });
-    //   // console.log(posts);
-    // };
+  // useEffect(() => {
+  //   // const getUserPosts = () => {
+  //   //   instance
+  //   //     .get(`/posts/userid`)
+  //   //     .then((res: any) => {
+  //   //       // handle success
+  //   //       // console.log(res.data);
+  //   //       setUserPosts(res.data);
+  //   //     })
+  //   //     .catch((error: any) => {
+  //   //       // handle error
+  //   //       console.log(error);
+  //   //     })
+  //   //     .finally(() => {
+  //   //       // always executed
+  //   //     });
+  //   //   // console.log(posts);
+  //   // };
 
-    const getUserPostApp = () => {
-      getUserPostApp();
-    };
-    getUserPostApp();
-  }, []);
+  //   const getUserPostApp = () => {
+  //     getUserPostApp();
+  //   };
+  //   getUserPostApp();
+  // }, []);
 
   return (
-    <div className="Home">
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              {userContext.user.username}
-            </Typography>
-          </Toolbar>
-        </AppBar>
+    <Box>
+      <Avatar
+        alt={userContext.user.username}
+        src={userContext.user.avatar}
+        sx={{ width: 200, height: 200, marginTop: "1rem" }}
+      ></Avatar>
+      <Box
+        display={"flex"}
+        justifyContent="center"
+        alignItems={"center"}
+        marginTop={"1rem"}
+      >
+        <Typography>Username</Typography>
+        <Button>
+          <Typography>Edit</Typography>
+          <EditIcon />
+        </Button>
       </Box>
+      {/* <div className="prof"> */}
 
-      <div className="prof">
-        <Avatar
-          alt={userContext.user.username}
-          src={userContext.user.avatar}
-          sx={{ width: 56, height: 56 }}
-        />
-
-        <div className="posts">
+      {/* <div className="posts">
           {userPosts.map((currPost) => (
             <Post key={currPost._id} post={currPost} />
           ))}
-        </div>
-      </div>
-    </div>
+        </div> */}
+      {/* </div> */}
+    </Box>
   );
 };
 
