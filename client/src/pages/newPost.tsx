63e1517541e477021d541029825c4e722e314041
unknown <danielnissan2002@gmail.com>
2025-02-23 20:38:55 +0200

    fix
    git commit -m fix

--- client/src/pages/newPost.tsx
+++ client/src/pages/newPost.tsx
@@ -27,18 +27,10 @@ const NewPost: React.FC = () => {
   const [previewImage, setPreviewImage] = useState<string | null>(null);
   const inputFileRef = useRef<HTMLInputElement>(null);
 
-  const {
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors },
-  } = useForm<FormData>();
-
   const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (!event.target.files || event.target.files.length === 0) return;
 
     const file = event.target.files[0];
-    setValue("img", event.target.files);
     setPreviewImage(URL.createObjectURL(file));
   };
 
@@ -68,106 +60,80 @@ const NewPost: React.FC = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          minHeight: "100vh",
-          gap: "5px",
-          paddingBottom: "60px", // Add padding to the bottom
+    // <form onSubmit={handleSubmit(onSubmit)}>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        minHeight: "100vh",
+        gap: "25px",
+        paddingBottom: "60px", // Add padding to the bottom
+      }}
+    >
+      {/* Image Upload */}
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        ref={inputFileRef}
+        className="inputfile"
+        style={{ visibility: "hidden" }}
+      />
+      <img
+        src={previewImage || picturePlaceHolder}
+        alt="Uploaded"
+        className="uploaded-image"
+        style={{ width: "200px", height: "200px", cursor: "pointer" }}
+        onClick={() => inputFileRef.current && inputFileRef.current.click()}
+      />
+
+      {/* Description */}
+      <TextField
+        sx={{ width: "100%", maxWidth: "400px" }}
+        label="Description"
+      />
+      {/* Location */}
+      <TextField
+        sx={{ width: "100%", maxWidth: "400px" }}
+        label="Location"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <LocationOn />
+            </InputAdornment>
+          ),
         }}
+      />
+
+      {/* Price */}
+      <TextField
+        sx={{ width: "100%", maxWidth: "400px" }}
+        label="Price"
+        type="number"
+        InputProps={{
+          startAdornment: <InputAdornment position="start">₪</InputAdornment>,
+        }}
+      />
+
+      {/* Submit Button - Always Visible */}
+      <Button
+        sx={{
+          mt: "20px",
+          width: "100%",
+          maxWidth: "400px",
+          backgroundColor: "rgb(235, 226, 226)",
+          color: "white",
+          position: "sticky",
+          bottom: "20px",
+        }}
+        type="submit"
+        className="post-button"
       >
-        {/* Image Upload */}
-        <input
-          type="file"
-          accept="image/*"
-          {...register("img", { required: "Image is required" })}
-          onChange={handleImageUpload}
-          ref={inputFileRef}
-          className="inputfile"
-          style={{ visibility: "hidden" }}
-        />
-        <img
-          src={previewImage || picturePlaceHolder}
-          alt="Uploaded"
-          className="uploaded-image"
-          style={{ width: "200px", height: "200px", cursor: "pointer" }}
-        />
-        <Box sx={{ height: "20px" }}>
-          {errors.img && <p className="error-message">{errors.img.message}</p>}
-        </Box>
-
-        {/* Description */}
-        <TextField
-          sx={{ width: "100%", maxWidth: "400px" }}
-          label="Description"
-          {...register("description", { required: "Description is required" })}
-        />
-        <Box sx={{ height: "20px" }}>
-          {errors.description && (
-            <p className="error-message">{errors.description.message}</p>
-          )}
-        </Box>
-
-        {/* Location */}
-        <TextField
-          sx={{ width: "100%", maxWidth: "400px" }}
-          label="Location"
-          {...register("location", { required: "Location is required" })}
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <LocationOn />
-              </InputAdornment>
-            ),
-          }}
-        />
-        <Box sx={{ height: "20px" }}>
-          {errors.location && (
-            <p className="error-message">{errors.location.message}</p>
-          )}
-        </Box>
-
-        {/* Price */}
-        <TextField
-          sx={{ width: "100%", maxWidth: "400px" }}
-          label="Price"
-          type="number"
-          {...register("price", {
-            required: "Price is required",
-            valueAsNumber: true,
-          })}
-          InputProps={{
-            startAdornment: <InputAdornment position="start">₪</InputAdornment>,
-          }}
-        />
-        <Box sx={{ height: "20px" }}>
-          {errors.price && (
-            <p className="error-message">{errors.price.message}</p>
-          )}
-        </Box>
-
-        {/* Submit Button - Always Visible */}
-        <Button
-          sx={{
-            mt: "20px",
-            width: "100%",
-            maxWidth: "400px",
-            backgroundColor: "#1976d2",
-            color: "white",
-            "&:hover": { backgroundColor: "#1565c0" },
-            position: "sticky",
-            bottom: "20px",
-          }}
-          type="submit"
-          className="post-button"
-        >
-          Post
-        </Button>
-      </Box>
-    </form>
+        Post
+      </Button>
+    </Box>
+    // </form>
   );
 };
 
