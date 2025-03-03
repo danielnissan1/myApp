b5a9e156af5d5458de645a7ebafe58df82b0561e
noasasson <noasasson18@gmail.com>
2025-03-03 19:15:56 +0200

    done

--- client/src/hooks/useAxiosPostRequests.ts
+++ client/src/hooks/useAxiosPostRequests.ts
@@ -2,16 +2,13 @@ import axios from "axios";
 import { FieldValues } from "react-hook-form";
 import { RoutesValues } from "../consts/routes";
 import { useNavigate } from "react-router-dom";
-import { formData } from "../components/Login/Register/formData";
 
 export const useAxiosPostRequests = () => {
   const navigate = useNavigate();
 
   const uploadImage = (file: File, url: string) => {
-    console.log("file:", file);
     return new Promise<string>((resolve, reject) => {
       const formData = new FormData();
-      console.log("form data:", formData);
 
       if (file) {
         formData.append("file", file);
@@ -23,12 +20,10 @@ export const useAxiosPostRequests = () => {
             withCredentials: true,
           })
           .then((res) => {
-            console.log(res);
             const url = res.data.url;
             resolve(url);
           })
           .catch((err) => {
-            console.log(err);
             reject(err);
           });
       }
@@ -40,7 +35,6 @@ export const useAxiosPostRequests = () => {
       data.profileImage[0],
       "http://localhost:3001/file"
     );
-    console.log("image url:", imgUrl);
 
     axios
       .post("http://localhost:3001/auth/register", {
