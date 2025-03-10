d8fba51a4fd0615138ddf2315abf713cb0c8950f
unknown <danielnissan2002@gmail.com>
2025-03-10 19:10:04 +0200

    enabeled

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,24 @@
-import initApp from "./server";
-const port = process.env.PORT;
+// import initApp from "./server";
+// const port = process.env.PORT;
 
-initApp().then((app) => {
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+// initApp().then((app) => {
+//   app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`);
+//   });
+// });
+
+import dotenv from "dotenv";
+import serverPromise from "./server";
+import { Server as HttpsServer } from "https";
+import { Server as HttpServer } from "http";
+dotenv.config();
+
+export type ServerType = HttpsServer | HttpServer;
+export type ServerInfo = { server: ServerType; port: number; link: string };
+
+serverPromise.then(({ server, port, link }: ServerInfo) => {
+  server.listen(port, () => {
+    console.log(`Server is running on ${link}/api`);
+    console.log(`Client is running on ${link}`);
   });
 });
